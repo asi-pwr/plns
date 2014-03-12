277af335e7a4e4bcb8e66fda6822cbee48c4ba50
Samaelson <paweuka@gmail.com>
2014-03-12 22:35:29 +0100

    New concept of Login View Class

--- plns/urls.py
+++ plns/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from plns.users.views import loginView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'plns.users.views.login'),
+    url(r'^login/$', loginView.as_view()),
 )
--- plns/users/views.py
+++ plns/users/views.py
@@ -1,27 +1,12 @@
-from django.template import RequestContext
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as auth_login
-from django.shortcuts import render_to_response
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 
 
+class loginView(FormView):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
 
-def login(request):
-    state = "Please log in below..."
-    email = password = ''
-    c= RequestContext(request)
-    if request.POST:
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user = authenticate(email=email, password=password)
-        if user is not None:
-            if user.is_active:
-                auth_login(request, user)
-                state = "You're successfully logged in!"
-            else:
-                state = "Your account is not active, please contact the site admin."
-        else:
-            state = "Your username and/or password were incorrect."
-
-    return render_to_response('login.html',{'state':state, 'email': email},c)
+    def form_valid(self, form):
+        login(self.request,form.get_user)
