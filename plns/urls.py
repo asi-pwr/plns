5207aeb6ff97ffeba7cce25b51b0f2b49c315dd2
Samaelson <paweuka@gmail.com>
2014-03-17 12:19:50 +0100

    Login View is Working(for me) /Mixin's TODO

--- plns/urls.py
+++ plns/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from plns.users.views import LoginView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', LoginView.as_view(success_url='/home/')),
     url(r'^categorys/$','plns.payments.views.show_categorys'),
 
 )
--- plns/users/views.py
+++ plns/users/views.py
@@ -4,9 +4,18 @@ from django.contrib.auth import login
 # Create your views here.
 
 
-class loginView(FormView):
+class LoginView(FormView):
     template_name = 'login.html'
     form_class = AuthenticationForm
-
     def form_valid(self, form):
-        login(self.request,form.get_user)
+        login(self.request, form.get_user())
+        return super(LoginView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(LoginView, self).form_invalid(form)
+
+    def dispatch(self, *args, **kwargs):
+        return super(LoginView, self).dispatch(*args, **kwargs)
+
+
+
