9f9b7d0498aaaf70449b99c34063f7c0031086f7
Azkel <s.michalsmyk@gmail.com>
2014-03-30 16:34:20 +0200

    Added type to models.py in plnsstats

--- plns/stats/models.py
+++ plns/stats/models.py
@@ -3,10 +3,12 @@ from django.utils.translation import ugettext as _
 
 
 class Chart(models.Model):
+    TYPES = ()
     name = models.CharField(_('Title'), max_length=300)
     config = models.TextField(_('Configuration String'))
     user = models.ForeignKey("users.User", verbose_name=_('User'))
     position = models.IntegerField(_('Position'))
+    type = models.IntegerField(choices=TYPES)
 
     def __unicode__(self):
         return self.name
