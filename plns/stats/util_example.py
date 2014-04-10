518d975fe8303f0fdd4e773595d5b043e7d2d90c
Azkel <s.michalsmyk@gmail.com>
2014-04-10 11:14:21 +0200

    Added example line chart.

---
+++ plns/stats/util_example.py
@@ -0,0 +1,51 @@
+import datetime
+from plns.payments import models
+from plns.stats.util import GraphMaker
+
+class LineGraphMaker(GraphMaker):
+    html_template = "<canvas id=\"oneChart\" width=\"500\" height=\"500\"></canvas>"
+
+    def __init__(self, username, chart_type, database, date_from, date_to):
+        self.username = username
+        self.chart_type = chart_type
+        self.database = database
+        self.date_from = date_from
+        self.date_to = date_to
+
+
+
+    def iterate_data(self):
+        """
+        Filters database with username, then data, and then by categories from chart_type Chart.config and returns
+        filtered entries
+        """
+        query = self.database.date_sort(self.date_from, self.date_to)
+        query= query.filter(user__exact=self.username)
+        return query
+
+    def generate(self):
+        """
+        Generates HTML that will be passed to view.
+        """
+        iterated = self.iterate_data()
+        chart_labels = []
+        chart_entries = []
+        js = """ var ctx = document.getElementById("oneChart").getContext("2d");"""
+        js += "var data = {\n	labels : ["
+        for e in iterated:
+            chart_entries.append(e.amount)
+            chart_labels.append(e.timestamp.hour+":"+e.timestamp.minute)
+        js.join(str(x)+"\",\"" for x in chart_entries)
+        js += """],\n
+            datasets : [\n
+            {\n
+            fillColor : "rgba(220,220,220,0.5)",\n
+            strokeColor : "rgba(220,220,220,1)",\n
+            pointColor : "rgba(220,220,220,1)",\n
+            pointStrokeColor : "#fff",\n
+            data : ["""
+        js.join(str(x)+"\",\"" for x in chart_entries)
+        js += """]\n
+        }\n]\n         }\n
+var myNewChart = new Chart(ctx).PolarArea(data);"""
+        return js, self.html_template
